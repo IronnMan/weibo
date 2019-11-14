9adb68ffd6ca05925cfbd45c9afd4c533144050a
wenlin <wenlin42@icloud.com>
2019-11-14 15:25:49 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.default')
+
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+    <h1>主页</h1>
+    <p class="lead">
+        Hello World
+    </p>
+    <p>一切，将从这里开始。</p>
+    <p>
+        <a href="#" role="button" class="btn btn-lg btn-success">现在注册</a>
+    </p>
+</div>
 @stop
