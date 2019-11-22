6e2223d9ea424e7492fa8a919533d07c82d38cab
wenlin <wenlin42@icloud.com>
2019-11-22 16:35:45 +0800

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,14 +1,29 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="jumbotron">
-    <h1>主页</h1>
-    <p class="lead">
-        Hello World
-    </p>
-    <p>一切，将从这里开始。</p>
-    <p>
-        <a href="{{ route('signup') }}" role="button" class="btn btn-lg btn-success">现在注册</a>
-    </p>
-</div>
+    @if (Auth::check())
+    <div class="row">
+        <div class="col-md-8">
+            <section class="status_form">
+                @include('shared._status_form')
+            </section>
+        </div>
+        <aside class="col-md-4">
+            <section class="user_info">
+                @include('shared._user_info', ['user' => Auth::user()])
+            </section>
+        </aside>
+    </div>
+    @else
+    <div class="jumbotron">
+        <h1>主页</h1>
+        <p class="lead">
+            Hello World
+        </p>
+        <p>一切，将从这里开始。</p>
+        <p>
+            <a href="{{ route('signup') }}" role="button" class="btn btn-lg btn-success">现在注册</a>
+        </p>
+    </div>
+    @endif
 @stop
