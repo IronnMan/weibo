294c7aace1dd53053cf98f549e9a67966f9356c5
wenlin <wenlin42@icloud.com>
2020-10-03 07:30:48 +0800

    初始化样式

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>@yield('title', 'Weibo App')</title>
+
+  <!-- Fonts -->
+  <link href="/css/app.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a href="/" class="navbar-brand">Weibo App</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a href="/help" class="nav-link"></a></li>
+        <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container content">
+    @yield('content')
+  </div>
+</body>
+
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+
+@section('content')
+  <div class="jumbotron">
+    <h1>Hello World</h1>
+    <p class="lead">
+      测试
+    </p>
+    <a href="#" role="button" class="btn btn-lg btn-success">现在注册</a>
+  </div>
+@stop
