248d3ec88cc04c711a612faf079ba7f5da5aa7cc
wenlin <wenlin42@icloud.com>
2020-10-05 03:47:01 +0800

    Move user model to models folder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Weibo') }}</title>
+    <title>{{ config('app.name', 'Weibo') }} - 随时随地发现新鲜事</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
